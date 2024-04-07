796de0eab3d211e1f5a7d7bb48ceb5c7fedac872
Cadu Moreira <msilveira.carlos@gmail.com>
2024-04-07 19:12:16 -0300

    Importando styles e html, adicionando pasta Static e rodando python manage collectstatic

--- setup/settings.py
+++ setup/settings.py
@@ -121,6 +121,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Colocar todos os arquivos estáticos nessa pasta
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'setup/static')
+]
+
+# Caminho onde todos os arquivos estáticos estão
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
